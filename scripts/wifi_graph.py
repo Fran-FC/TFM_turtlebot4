60a58db924be4f8ece08a535b0ba5c0cd0370197
Fran-FC <frafolcm@gmail.com>
2023-07-09 15:50:33 +0000

    added remote and local options

--- scripts/wifi_graph.py
+++ scripts/wifi_graph.py
@@ -2,22 +2,34 @@ import paramiko, re
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from collections import deque
+import argparse
+import subprocess
 
 
 
-# SSH connection details
-host = '10.42.0.21'
-port = 22
-username = 'ubuntu'
+parser = argparse.ArgumentParser()
+parser.add_argument("--remote_connection", default=False, help="Measure wifi connectivity from a remote machine")
+parser.add_argument("--remote_ip", default="10.42.0.21")
+parser.add_argument("--remote_username", default="ubuntu")
+parser.add_argument("--interface", default="wlo1")
+args = parser.parse_args()
+
+if args.remote_connection:
+    # SSH connection details
+    host = '10.42.0.21'
+    port = 22
+    username = 'ubuntu'
+
+    # Create an SSH client
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    # Connect to the remote server using the identity file
+    client.connect(hostname=host, username=username)
+    
 
 # Remote command
-command = "iwconfig wlan0 | grep -i quality"
+command = "iwconfig {} | grep -i quality".format(args.interface)
 
-# Create an SSH client
-client = paramiko.SSHClient()
-client.load_system_host_keys()
-# Connect to the remote server using the identity file
-client.connect(hostname=host, username=username)
 
 max_length = 500
 # Create a deque with a maximum size of 10
@@ -38,8 +50,13 @@ line1, = ax.plot([], [], lw=2, label="Quality (x/70)")
 ax.legend(loc='upper left')
 
 def update(i):
-    stdin, stdout, stderr = client.exec_command(command)
-    output = stdout.read().decode()
+    if args.remote_connection:
+        stdin, stdout, stderr = client.exec_command(command)
+        output = stdout.read().decode()
+    else:
+        output = subprocess.check_output(command, shell=True).decode("utf-8")
+
+    print("output: %s" % output)
     
     level = int(re.findall(r"(\d+)\s+dBm", output)[0])
     level = 1000/level
